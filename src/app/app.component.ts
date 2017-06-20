f8d2f573b232d07645f679d3dab0ecdf8336df00
Caleb Elliott <calebelliott070@gmail.com>
2017-06-20 10:59:50 -0600

    Finished the assignment

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  projectForm: FormGroup;
+
+  invalidProjectName(control: FormControl): {[s: string]: boolean} {
+    if (control.value === 'Test') {
+      return {'invalidProjectName': true};
+    }
+    return null;
+  }
+
+  asyncInvalidProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'TestProject') {
+          resolve({'invalidProjectName': true});
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    })
+    return promise;
+  }
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName' : new FormControl(
+        null,
+        [Validators.required, this.invalidProjectName],
+        this.asyncInvalidProjectName
+      ),
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus' : new FormControl('stable'),
+    });
+  }
+
+  onCreateProject(){
+    console.log(this.projectForm.value);
+  }
+}
